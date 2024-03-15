f2c747b33da6ac5c6feef97d173f7d9eeedfa546
Akash LM <97584884+akash4sh@users.noreply.github.com>
2024-03-15 22:17:05 +0530

    Create schema.sql

---
+++ ratingservice/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE bookstorebackend;
+USE bookstorebackend;
+CREATE TABLE [dbo].[Rating](
+    [Review] [nvarchar](100) NULL,
+    [Rating] [int] NULL,
+    [BookID] [int] NULL,
+    [UserID] [int] NULL,
+CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED
+(
+    [id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY];
