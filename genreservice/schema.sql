23b405570c5bd6d3b9a4b5bb27a75b451170ef9d
Akash LM <97584884+akash4sh@users.noreply.github.com>
2024-03-15 22:14:12 +0530

    Create schema.sql

---
+++ genreservice/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE bookstorebackend;
+USE bookstorebackend;
+CREATE TABLE [dbo].[Genre](
+    [Name] [nvarchar](50) NULL,
+    [Keywords] [nvarchar](255) NULL,
+    [Suggested] [bit] NOT NULL,
+CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED
+(
+    [id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY];
